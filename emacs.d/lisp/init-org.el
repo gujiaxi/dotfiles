40f25ffc7ac79aaf2d3f4326485e61309052e70f
Isaac <imjiaxi@gmail.comm>
2016-04-16 00:37:24 +0800

    .

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -21,7 +21,7 @@
          :base-extension "org"
          :publishing-directory "~/.org/html/"
          :publishing-function org-html-publish-to-html
-         :exclude "todo.org"
+         :exclude "todo.org\\|.*-p.org"
          :with-toc t
          :html-head-include-default-style nil
          :html-head "<link rel='stylesheet' href='./css/org.css' type='text/css'/>"
