cfc29ef27531de804271a75e5c091ef8b56c5e31
gujiaxi <imjiaxi@gmail.com>
2016-08-04 15:09:37 +0800

    remove rtags package

--- emacs.d/lisp/init-clang.el
+++ emacs.d/lisp/init-clang.el
@@ -1,19 +1,21 @@
-;;---------------------------------------------------------------------------
+;;--------------------------------------------------------------------------
 ;; clang conf
-;;---------------------------------------------------------------------------
+;;--------------------------------------------------------------------------
 
-
-;; rtags
-(require-package 'rtags)
-(rtags-enable-standard-keybindings)
-(setq rtags-autostart-diagnostics t)
-(setq rtags-use-helm t)
 
 ;; irony-mode
 (require-package 'irony)
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
+
+;; company-irony
+(require-package 'company-irony)
+(add-to-list 'company-backends 'company-irony)
+
+;; company-c-headers
+(require-package 'company-c-headers)
+(add-to-list 'company-backends 'company-c-headers)
 
 
 (provide 'init-clang)
