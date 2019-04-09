f51042f0c0184233d72c24c571610f3415d56956
Jiaxi Gu <imjiaxi@gmail.com>
2019-04-09 15:59:42 +0800

    use a single init.lua for hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,12 +1,44 @@
--- Key definition
+----------------------
+--/ Key Definition /--
+----------------------
 hyper = {'ctrl', 'alt', 'cmd'}
 hyperShift = {'ctrl', 'alt', 'cmd', 'shift'}
 
--- App launcher
-require('app-launcher')
+--------------------
+--/ App Launcher /--
+--------------------
+local key2app = {
+   c = 'Calendar',
+   e = 'Emacs',
+   f = 'Finder',
+   i = 'iTunes',
+   m = 'Mail',
+   n = 'Notes',
+   r = 'Reminders',
+   s = 'Safari',
+   t = 'Terminal',
+   w = 'WeChat'
+}
+for key, app in pairs(key2app) do
+   hs.hotkey.bind(hyper, key, nil, function() hs.application.launchOrFocus(app) end)
+end
 
--- Window resizer
-require('window-resizer')
-
--- Reload hs config
-hs.hotkey.bind(hyper, 'escape', nil , function() hs.reload() end)
+----------------------
+--/ Window Resizer /--
+----------------------
+function push(x, y, w, h)
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w*x)
+  f.y = max.y + (max.h*y)
+  f.w = max.w*w
+  f.h = max.h*h
+  win:setFrame(f)
+end
+hs.window.animationDuration = 0
+hs.hotkey.bind(hyper, "left", function() push(0, 0, 0.5, 1) end)
+hs.hotkey.bind(hyper, "right", function() push(0.5, 0, 0.5, 1) end)
+hs.hotkey.bind(hyper, "up", function() push(0, 0, 1, 1) end)
+hs.hotkey.bind(hyper, "down", function() push(0.1, 0.1, 0.52, 0.6) end)
